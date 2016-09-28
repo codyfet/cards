509ce96af4e0848e058d9708213ad1e8577a18ce
Alexander Volkov <codyfet@mail.ru>
2016-09-28 18:05:33 +0300

    Added validation for field on reg and log forms

--- assets/js/cardsApp/views/mainView.js
+++ assets/js/cardsApp/views/mainView.js
@@ -8,7 +8,10 @@ cardsApp.MainView = Backbone.View.extend({
         "click #create-account": "showRegbox",
         "click #sign-in": "showLogbox",
         "click .enter-btn": "loginFunc",
-        "click .register-btn": "registerFunc"
+        "click .register-btn": "registerFunc",
+
+        "focusout input": "focusoutField",
+        "focus input": "focusField"
     },
 
     initialize: function() {
@@ -22,37 +25,48 @@ cardsApp.MainView = Backbone.View.extend({
     },
 
     showRegbox: function() {
+        this.clearAllFields();
         this.$el.find("#logbox").hide();
         this.$el.find("#regbox").show();
     },
 
     showLogbox: function() {
+        this.clearAllFields();
         this.$el.find("#logbox").show();
         this.$el.find("#regbox").hide();
     },
 
-    registerFunc: function() {
+    clearAllFields: function(){
+        this.$el.find("#logbox, #regbox").find("input").val("").removeClass("invalid");
+    },
 
+    registerFunc: function() {
         var userData = {
             username: $("#regbox input[name='user[name]']").val(),
             password: $("#regbox input[name='user[password]']").val(),
             email: $("input[name='user[email]']").val()
         }
-
-        this.registerUser(userData);
+        if(this.validateRegForm()==true){
+            this.registerUser(userData);
+        }
+        else {
+            $("#status").html("вы ввели некорректные значения");
+        }
     },
 
     loginFunc: function() {
-
         var userData = {
             //username : $("#logbox input[name='user[name]']").val(),
             password: $("#logbox input[name='user[password]']").val(),
             email: $("#logbox input[name='user[email]']").val()
         }
-
-        this.loginUserAsync(userData);
+        if(this.validateLogForm()==true){
+            this.loginUserAsync(userData);
+        }
+        else {
+            $("#status").html("вы ввели некорректные значения");
+        }
     },
-
 
     // BACKENDLESS REGISTRATION FUNCTIONS
     registerUser: function(userData) {
@@ -60,6 +74,7 @@ cardsApp.MainView = Backbone.View.extend({
         user.email = userData.email;
         user.password = userData.password;
         user.name = userData.username;
+
         $("#status").html("регистрация пользователя...");
         Backendless.UserService.register(user, new Backendless.Async(this.userRegistered, this.gotError));
     },
@@ -98,6 +113,86 @@ cardsApp.MainView = Backbone.View.extend({
         console.log("Server reported an error - ");
         console.log(backendlessFault.message);
         console.log(backendlessFault.statusCode);
+    },
+
+    validateRegForm: function(){
+        var that = this;
+        var inputs = $("#regbox form input");
+        var isFormValid = true;
+        $.each(inputs, function(index, el){
+            var isFieldValid = that.validateField(el);
+            if(isFieldValid==false){
+                isFormValid=false;
+            }
+        });
+        return isFormValid;
+    },
+
+    validateLogForm: function(){
+        var that = this;
+        var inputs = $("#logbox form input");
+        var isFormValid = true;
+        $.each(inputs, function(index, el){
+            var isFieldValid = that.validateField(el);
+            if(isFieldValid==false){
+                isFormValid=false;
+            }
+        });
+        return isFormValid;
+    },
+
+    focusoutField: function(e){
+        var element = e.target;
+        this.validateField(element);
+    },
+
+    validateField: function(element){
+        //var element = e.target;
+        var isValid = true;
+        switch(element.name){
+            case "user[name]":
+                // required
+                if(element.value==""){
+                    $(element).addClass("invalid");
+                    isValid=false;
+                }
+                break;
+            case "user[password]":
+                // required
+                if(element.value==""){
+                    $(element).addClass("invalid");
+                    isValid=false;
+                }
+                break;
+            case "user[password2]":
+                // required
+                // equals to password
+                if(element.value=="" || element.value!=$("input[name='user[password]']").val()){
+                    $(element).addClass("invalid");
+                    isValid=false;
+                }
+                break;
+            case "user[email]":
+                // required
+                // email format
+                if(element.value=="" || !this.isValidEmailAddress(element.value)){
+                    $(element).addClass("invalid");
+                    isValid=false;
+                }
+                break;
+        }
+        return isValid;
+
+    },
+
+    focusField: function(e){
+        // remove red border on focus event
+        $(e.target).removeClass("invalid");
+    },
+
+    isValidEmailAddress: function(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
     }
 
 });
