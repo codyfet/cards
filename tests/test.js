87fd8f07da5b11967235613b9915a1b8a882322b
Podolyan <vladimir.podolyan@accenture.com>
2016-09-30 15:30:44 +0300

    Small test reactoring

--- tests/test.js
+++ tests/test.js
@@ -9,12 +9,10 @@ QUnit.test( "hello test", function( assert ) {
 
 QUnit.test("Email validator should add class invalid to the input", function(assert) {
     F.open("../index.html", function () {
-        var emailInput = F("input.email");
-        emailInput.visible(function (el) {
+        F("input.email").visible(function (el) {
             el.type("test");
-            var ll = el;
             el.next().click(function() {
-                assert.ok(ll.hasClass("invalid"));
+                assert.ok(el.hasClass("invalid"));
             });
         });
     });
