0d31760252aa6583747832ed0798dc2e10248bba
Alexander Volkov <codyfet@mail.ru>
2016-10-07 16:19:12 +0300

    refactored login/register functionalities

--- assets/js/cardsApp/views/mainView.js
+++ assets/js/cardsApp/views/mainView.js
@@ -12,6 +12,7 @@ cardsApp.MainView = Backbone.View.extend({
 
         "focusout input": "focusoutField",
         "focus input": "focusField"
+
     },
 
     initialize: function() {
@@ -20,7 +21,14 @@ cardsApp.MainView = Backbone.View.extend({
             if(!$(this).hasClass("empty-avatar")){
                 router.navigate('account', true);
             }
-        })
+        });
+        var that = this;
+        $(".login-header").click(function(){
+            that.openLoginModal();
+        });
+        $(".register-header").click(function(){
+            that.openRegisterModal();
+        });
     },
 
     render: function() {
@@ -111,7 +119,8 @@ cardsApp.MainView = Backbone.View.extend({
         appModel.set("loggedUser", loggedInUser);
         router.navigate('cardsTable', true);
         $(".user-pic").removeClass("empty-avatar");
-        $(".navbar-nav").find("li").show();
+        $(".navbar-nav.logged-header").show();
+        $(".navbar-nav.not-logged-header").hide();
     },
 
     handleFault: function(backendlessFault) {
@@ -198,6 +207,19 @@ cardsApp.MainView = Backbone.View.extend({
     isValidEmailAddress: function(emailAddress) {
         var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         return pattern.test(emailAddress);
+    },
+
+    openRegisterModal: function(){
+        $('#registerModal').on('shown.bs.modal', function () {
+            $(this).find('input').first().focus();
+        });
+        $('#registerModal').modal('show');
+    },
+    openLoginModal: function(){
+        $('#loginModal').on('shown.bs.modal', function () {
+            $(this).find('input').first().focus();
+        });
+        $('#loginModal').modal('show');
     }
 
 });
